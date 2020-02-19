7813ae81ebf9e4a2b1a7a230ec6604cd6ccb39cd
John Doe <p.mafungwa@gmail.com>
2020-02-19 08:29:51 +0200

    mastr

---
+++ Assignment1/src/main/java/cput/ac/za/calc.java
@@ -0,0 +1,12 @@
+package cput.ac.za;
+
+public class calc {
+    public void bala(){
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+
+}
---
+++ Assignment1/src/test/java/cput/ac/za/calcTest.java
@@ -0,0 +1,22 @@
+package cput.ac.za;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class calcTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void bala() {
+    }
+}
